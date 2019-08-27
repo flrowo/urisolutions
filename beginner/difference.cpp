b1bc084351b91cc18cd1713256eb01f6d483c663
João Pedro Libonati <49959057+funkymatoi@users.noreply.github.com>
2019-08-27 05:43:45 +0000

    Difference

---
+++ beginner/difference.cpp
@@ -0,0 +1,12 @@
+// simple as asked
+#include <iostream>
+using namespace std;
+int main() {
+	int A, B, C, D;
+	cin >> A;
+	cin >> B;
+	cin >> C;
+	cin >> D;
+	cout << "DIFERENCA = " << A*B-C*D << endl;
+	return 0;
+}
