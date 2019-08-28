85f860c4387108c74aa010856f01a05009f386a1
João Pedro Libonati <49959057+funkymatoi@users.noreply.github.com>
2019-08-28 16:12:37 +0000

    Product by Categories

--- sql/productsbycategories.sql
+++ sql/productsbycategories.sql
@@ -1,2 +1,2 @@
 select c.name as "name", sum(p.amount) as "sum" --name of the tables as "name" and "sum"
-from products as p, categories as c where p.id_categories = c.id group by c.name --need to be c.name here
+from products as p, categories as c where p.id_categories = c.id group by c.name --c.name not c.id
